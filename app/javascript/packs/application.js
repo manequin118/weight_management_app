dfff999b9bda85a3d8be3db3a4f37fc929016ffc
chu <72890669+manequin118@users.noreply.github.com>
2021-03-19 06:12:52 +0900

    devise、ナビバー,

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,25 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+// Bootstrap
+require("bootstrap/dist/js/bootstrap");
+
+// Chart.js
+require("chart.js");
+
+// カレンダー
+require("flatpickr");
+require("flatpickr/dist/l10n/ja");
+// カレンダーの色を変更
+require("flatpickr/dist/themes/material_blue.css");
